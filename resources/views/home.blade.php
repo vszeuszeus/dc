2905de72bfc93bd1ae031123bff864198bc0171a
makhataibar <makhataibar@gmail.com>
2018-06-20 09:57:36 +0600

    add responsive to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,45 @@
 
                                     <div class="card-body">
 
-                                        <div class="btn-group" role="group"
-                                             aria-label="Button group with nested dropdown">
+                                        <div class="btn-group d-none d-sm-inline-flex" role="group">
+                                            <div class="btn-group" role="group">
+                                                <button id="btnGroupDrop1" type="button"
+                                                        class="btn btn-secondary @if(!$category->lectures->count()) disabled @endif dropdown-toggle"
+                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    Лекции
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                                    @foreach($category->lectures as $lecture)
+                                                        <a class="dropdown-item"
+                                                           href="{{route('home.readLecture', $lecture->id)}}">{{$lecture->title}}</a>
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                            @if(!$category->lectures->count())
+                                                <button disabled
+                                                        href="{{route('home.beginTest', ['type' => 'LectureCategory', 'id' => $category->id])}}"
+                                                        type="button"
+                                                        class="btn btn-secondary">Итоговый тест
+                                                </button>
+                                            @else
+                                                <a href="{{route('home.beginTest', ['type' => 'LectureCategory', 'id' => $category->id])}}"
+                                                   type="button"
+                                                   class="btn btn-secondary">Итоговый тест</a>
+                                            @endif
+
+                                            @if($category->checkPassedTest())
+                                                <a disabled href="{{route('home.getCertificate', $category->id)}}"
+                                                   class="btn btn-secondary">Получить сертификат</a>
+                                            @else
+                                                <button disabled href="{{route('home.getCertificate', $category->id)}}"
+                                                        class="btn btn-secondary">Получить сертификат
+                                                </button>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="btn-group-vertical d-inline-flex d-sm-none" role="group">
                                             <div class="btn-group" role="group">
                                                 <button id="btnGroupDrop1" type="button"
                                                         class="btn btn-secondary @if(!$category->lectures->count()) disabled @endif dropdown-toggle"
